7bba548fd2c5532b93ac2d9694616c53ae493629
Bokang <sefali.bka@gmail.com>
2025-12-14 22:29:19 +0200

    Add a in the code

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -5,7 +5,7 @@ if weather == "sunny" :
 elif weather == "rainy":
     print("Dont forget your umbrella and a raincoat.")
 elif weather == "cold":
-    print("Make sure to wear a warm coat and scarf.")
+    print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I dont have recommendations for this weather.")
 
