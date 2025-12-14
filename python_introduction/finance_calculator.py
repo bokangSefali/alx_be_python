2c498fdf26b9f6244da81788b87f1e53c780ff4f
Bokang <sefali.bka@gmail.com>
2025-12-14 15:28:31 +0200

    Underscore instead of camel case

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,9 @@
-monthlyIncome = int(input("Enter your montly income: "))
-monthlyExpenses = int(input("Enter your montly expenses: "))
+monthly_income = float(input("Enter your montly income: "))
+monthly_expenses = float(input("Enter your montly expenses: "))
 
-monthlySavings = monthlyIncome - monthlyExpenses
+monthly_savings = monthly_income - monthly_expenses
 
-projectedSavings = (monthlySavings * 12 + (monthlySavings * 12 * 0.05))
+projected_savings = (monthly_savings * 12 + (monthly_savings * 12 * 0.05))
 
-print("Your montly savings are $",monthlySavings)
-print("Projected savings after one year, with interest, is: $",projectedSavings)
+print("Your montly savings are $",monthly_savings)
+print("Projected savings after one year, with interest, is: $",projected_savings)
